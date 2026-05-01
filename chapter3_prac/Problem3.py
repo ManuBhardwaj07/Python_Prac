308031c660abfc400dfe0a3378c4783469be9f48
Manu Bhardwaj <bhardwajkhuman@gmail.com>
2026-05-01 14:21:24 +0530

    Add files via upload

---
+++ chapter3_prac/Problem3.py
@@ -0,0 +1,8 @@
+# 3. Write a program to detect double space in a string.
+
+name= "Manu is a good boy And "
+print(name.replace("  ","  "))
+
+
+
+print(name)     # String are immutable which means that you cannot change them by running functions on them
---
+++ chapter3_prac/Problem5.py
@@ -0,0 +1,4 @@
+# 5. Write a program to format the following letter using escape sequence characters.
+
+letter = "Dear Harry,\n\tthis python course is nice.\nthanks!"
+print(letter)
