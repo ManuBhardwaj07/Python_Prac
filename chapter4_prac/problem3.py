788ec15e2a0531866c9c5bc3c6e1944aca61195d
Manu Bhardwaj <bhardwajkhuman@gmail.com>
2026-05-05 20:48:14 +0530

    Add files via upload

---
+++ chapter4_prac/problem3.py
@@ -0,0 +1,3 @@
+# Check that a tuple  type cannot be changed in Python.
+a= (35 ,"Manu" , )
+a[2]= "marry"
---
+++ chapter4_prac/problem4.py
@@ -0,0 +1,5 @@
+# Write a program to sum a list with 4 numbers.
+
+L= [1,5,6,257]
+
+print(sum(L))
