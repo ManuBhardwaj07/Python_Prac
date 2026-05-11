7fe633e194c5daa7b2969282fe8c85d08a742b77
Manu Bhardwaj <bhardwajkhuman@gmail.com>
2026-05-12 01:15:02 +0530

    Add files via upload

---
+++ chapter_6/conditional.py
@@ -0,0 +1,5 @@
+a = 60
+if (a>60):
+    print("greater ")
+else:
+    print("smaller")
---
+++ chapter_6/if_else.py
@@ -0,0 +1,13 @@
+a= int(input("enter the number"))
+
+#  if_else statement
+if (a>60):
+    print("greater ")
+    print("you are above 60")
+
+else:
+    print("smaller")
+
+
+
+print("code ends here")
